3d7ab3e0a3f326c5bd6a65e2a8cf859caa66d831
egorovaelena2987 <165517331+egorovaelena2987@users.noreply.github.com>
2024-09-22 16:40:07 -0500

    Add files via upload

---
+++ src/FetchMeals.js
@@ -0,0 +1,34 @@
+import axios from 'axios';
+
+const getAllMeals = (setMeal) => {
+    axios.get("http://localhost:4000")
+    .then(({data}) => {console.log(data)
+        setMeal(data)
+    })
+}
+const addMeal = (name, setName, setMeal) => {
+    axios.post('http://localhost:4000/saveMeals', {name})
+    .then((data) => {
+        console.log(data);
+        setName("");
+        getAllMeals(setMeal)
+    })
+}
+const editMeal = (mealId, name, setName, setMeal, setEditing) => {
+    axios.put('http://localhost:4000/editMeal', {_id:mealId, name})
+    .then((data) => {
+        console.log(data);
+        setName("");
+        setEditing(false)
+        getAllMeals(setMeal)
+    })
+}
+const deleteMeal = (_id, setMeal) => {
+    axios.delete('http://localhost:4000/deleteMeal', {_id})
+    .then((data) => {
+        console.log(data);
+        getAllMeals(setMeal)
+    })
+}
+
+export {getAllMeals, addMeal, editMeal, deleteMeal}
